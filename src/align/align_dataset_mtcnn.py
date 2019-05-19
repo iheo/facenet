6f4a5271f6f257f3cfaa91d6ff2f98d3bb03d7f4
Inseok Heo <inseok@amazon.com>
2019-05-19 10:10:23 -0400

    initial commit and added sys path for python path

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -28,6 +28,7 @@ from __future__ import print_function
 from scipy import misc
 import sys
 import os
+sys.path.append(os.path.expanduser('~/projects/facenet/src'))
 import argparse
 import tensorflow as tf
 import numpy as np
